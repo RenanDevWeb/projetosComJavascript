1407275efc329997895e0c08c67d60e552690b4d
Renan <renansalmazio@gmail.com>
2023-10-16 20:15:37 -0300

    arquivos do projeto

--- cauculadoraDeGorgeta/script.js
+++ cauculadoraDeGorgeta/script.js
@@ -7,20 +7,33 @@ function cauculateTip(){
  const billAmmout = parseFloat(document.querySelector("#billAmount").value)
  const serviceQuality = parseFloat(document.querySelector("#serviceQuality").value)
 
- const tipAmmoutElement = document.querySelector("#tipAmount")
- const totalAmountElement = document.querySelector("#totalAmount")
 
- if(billAmmout === ""){ 
+  console.log(billAmmout);
+
+ if(isNaN(billAmmout)){ 
     alert("Dados vazios")
     return; 
 }
+  
+
+
+
 
  const tipAmmout  = billAmmout * serviceQuality
  const totalAmount = billAmmout + tipAmmout
 
+  const tipAmmoutText =  tipAmmout.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+  const totalAmountText =  totalAmount.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
 
-  tipAmmoutElement.value = tipAmmout
-  totalAmountElement.value = totalAmount
+ console.log(tipAmmoutText,totalAmountText);
+
+
+ const tipAmmoutElement = document.querySelector("#tipAmount")
+ const totalAmountElement = document.querySelector("#totalAmount")
+
+
+  tipAmmoutElement.value = tipAmmoutText
+  totalAmountElement.value = totalAmountText
 
 }
 
